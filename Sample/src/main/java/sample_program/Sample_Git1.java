170e6c4d30fdac9cd71615d21b8599feb6ed1c3c
sajit <sajitha0628@gmail.com>
2023-11-27 20:54:55 -0500

    my first commit

---
+++ Sample/src/main/java/sample_program/Sample_Git1.java
@@ -0,0 +1,10 @@
+package sample_program;
+
+public class Sample_Git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
